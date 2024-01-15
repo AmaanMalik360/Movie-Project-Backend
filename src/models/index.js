1f88f00fff87cd96ea45f5d14f6d9102691e2e4a
Amaan Malik <amaanmalik0360@gmail.com>
2024-01-15 19:07:51 +0500

    changed connection configurations

--- src/models/index.js
+++ src/models/index.js
@@ -9,11 +9,15 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(process.env.db_schema, process.env.db_username, process.env.db_password, {
+    ...config,
+    host: process.env.db_host || config.host // Use process.env.db_host if available, otherwise fall back to config.host
+  });
 }
 
 fs
